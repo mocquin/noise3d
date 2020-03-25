cd13a2789cbc71f427ad947d86d5a7d1aaf16f7e
mocquin <mocquin@me.com>
2020-03-25 15:31:37 +0100

    update imports

--- noise3d/__init__.py
+++ noise3d/__init__.py
@@ -1,5 +1,7 @@
 from .genseq import genseq_3dnoise_seq
 
-from .opr import get_all_3d_noise_seq
+from .opr import get_all_3d_noise_seq_classic
 
-from .noise import get_all_3d_noise_var, get_all_3d_noise_var_fast, get_all_3d_corrected_var_matrix
+from .noise import get_all_3d_noise_var_classic, get_all_3d_noise_var_fast, get_all_3d_corrected_var_matrix
+
+from . import disp
